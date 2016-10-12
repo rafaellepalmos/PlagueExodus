f0ac1596fbf2cda7094ecf058098ceb176145b5d
Latte <tae_synchronize@hotmail.com>
2016-10-13 04:21:44 +0700

    Pallarex Background at Menu State

--- fayousStateNOT/menuState.js
+++ fayousStateNOT/menuState.js
@@ -1,8 +1,10 @@
 var menuState = {
 	create: function() {
-		var bgImage = game.add.sprite(0, 0, 'menuBackground');
-		bgImage.width = game.width;
-		bgImage.height = game.height;
+//		var bgImage = game.add.sprite(0, 0, 'menuBackground');
+		this.bgImage = game.add.tileSprite(-15, 0, game.width+50, game.height+50, 'menuBackground');
+		this.bgImage.tileScale.set(0.35);
+		this.bgImage.countMove = 0;
+		this.bgImage.direction = 'right';
 
 		var logo = game.add.sprite(game.width/2, 100, 'logo');
 		logo.anchor.setTo(0.5, 0.5);
@@ -30,5 +32,23 @@ var menuState = {
 		this.bgMusic.stop();
 		game.state.start('play');
 //		game.state.start('gameover'); //test gameover state
+	},
+
+	update: function() {
+		if (this.bgImage.countMove < 59 && this.bgImage.direction == 'right') {
+			this.bgImage.tilePosition.set(this.bgImage.x + this.bgImage.countMove, 0);
+			this.bgImage.countMove = this.bgImage.countMove+0.06;
+		}
+		if (this.bgImage.countMove > 0 && this.bgImage.direction == 'left') {
+			this.bgImage.tilePosition.set(this.bgImage.x + this.bgImage.countMove, 0);
+			this.bgImage.countMove = this.bgImage.countMove-0.06;
+		}
+		if (this.bgImage.countMove > 59) {
+			this.bgImage.direction = 'left';
+		}
+		else if (this.bgImage.countMove < 0) {
+			this.bgImage.direction = 'right';
+		}
+
 	}
 };
