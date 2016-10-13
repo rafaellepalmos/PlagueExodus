10c1181a53b4fb013722c083d58488d92ab6c1cf
rafaellepalmos <rafaelle.palmos@students.stamford.edu>
2016-10-13 09:29:48 +0700

    pause

--- fayousStateNOT/videoState.js
+++ fayousStateNOT/videoState.js
@@ -9,7 +9,15 @@ var videoState = {
 		this.video = game.add.video('video');
 		this.video.addToWorld(game.width/2,game.height/2,0.5,0.5,0.60,0.60);
 		this.video.onComplete.add(this.change, this);
-		this.video.play(false);
+		this.video.play(false);//cursor input
+		this.cursor = game.input.keyboard.createCursorKeys();
+	},
+	
+	update: function(){
+		if (this.cursor.up.isDown) {
+			this.video.stop();
+			this.change();
+		}
 	},
 	
 	change: function(){
